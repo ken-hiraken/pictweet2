006731a93cef6e8e423fc0530d9bb7739994fdc1
ken-hiraken <kenichiro1030@gmail.com>
2016-07-12 20:18:51 +0900

    投稿画面のコントローラーとルーティング作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,5 @@
 class TweetsController < ApplicationController
 
-<<<<<<< HEAD
     def index
       @tweets = Tweet.all
     end
@@ -8,14 +7,4 @@ class TweetsController < ApplicationController
     def new
     end
 
-    def create
-      Tweet.create(name: "", image: "", text: "")
-    end
-
   end
-=======
-  def index
-  end
-
-end
->>>>>>> 6a0e3444fb44c17fcd958ad7b287aba93c217f1f
